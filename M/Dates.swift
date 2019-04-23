14c42f8e8f4799d11710136ef3815938bc065056
gladiusKatana <garthsnyder@protonmail.com>
2019-04-22 21:54:53 -0400

    Renamed groups; some moving; new file DateComponents.swift

--- M/Dates.swift
+++ M/Dates.swift
@@ -22,35 +22,12 @@ func createDate(_ year: Int, monthInt: Int, dayInt: Int) -> Date {
 }
 
 
-func hourOf(_ date: Date) -> Int {
-    let dateFormatter = DateFormatter();  dateFormatter.dateFormat = "HH" // HH is 24-hr; hh is 12-hr; ha is 12-hr showing am/pm
-    let hour = Int(dateFormatter.string(from: date))
-    return hour! //!  MAY CRASH HERE DEPENDING ON 24HR TIME SETTING & LOCATION (IE IF NOT ON 24HR & LOCATION SET TO UK) !!! (is this still true?)
-}
-
-func dayIntOf(_ date: Date) -> Int {
-    let dateFormatter = DateFormatter();  dateFormatter.dateFormat = "d"
-    let day = Int(dateFormatter.string(from: date))
-    return day!
-}
-
-func monthNameOf(_ date: Date) -> String {                           // below methods basically return same as above one, but selectively
-    let dateFormatter = DateFormatter();  dateFormatter.dateFormat = "MMMM"
-    let nameOfMonth = dateFormatter.string(from: date)
-    return nameOfMonth
-}
-
-func yearOf(_ date: Date) -> Int {
-    let dateFormatter = DateFormatter();  dateFormatter.dateFormat = "YYYY"
-    let year = Int(dateFormatter.string(from: date))
-    return year!
-}
-
 
 func formalDateString(_ date: Date, comment: String) -> String {
     let (wkday, monString, dayI, yr) = displayDate(date)
     return "\(comment) \(wkday), \(monString) \(dayI), \(yr)"
 }
+
 
 func displayDate(_ inputDate: Date)
     -> (weekday: String, monthStr: String, dayInt: Int, year: Int)
