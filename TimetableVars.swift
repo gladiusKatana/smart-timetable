a705428878370d9c1aea2814dff67daae4e9ec00
gladiusKatana <garthsnyder@protonmail.com>
2019-05-01 22:37:06 -0400

    More reorg

--- TimetableVars.swift
+++ TimetableVars.swift
@@ -4,7 +4,6 @@
 import UIKit
 
 var currentDate = Date()
-
 var lastLoggedInDate = Date()
 
 var (year, month, day, weekday, hour, minute) = (0, "", 0, "", 0, 0)
@@ -19,6 +18,10 @@ var wkdysDefaultOrder = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
 
 var hoursOfTheDay = ["12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11"]
 //var militaryDayHours = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23"]
+
+var eventsAtIndexPath = Dictionary<IndexPathDictionary<Int,Int>,[String]>()
+
+var selectedPath = [0, 0]
 
 var nowRow = 0;       var nowColumn = 0
 
