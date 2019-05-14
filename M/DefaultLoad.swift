95c23535bf572d299c3eb559652f91bb2e05fd6b
gladiusKatana <garthsnyder@protonmail.com>
2019-05-14 10:22:34 -0400

    Slight cleanup of date logic in DefaultLoad; console-cleanup/aesthetic changes

--- M/DefaultLoad.swift
+++ M/DefaultLoad.swift
@@ -2,14 +2,11 @@
 //  smart-timetable  ∙  1st commit Apr. 07, 2019  ∙  Created by Garth Snyder (a.k.a. gladiusKatana ⚔️)
 import UIKit
 
-func defaultLoad(showDate: Bool) {                                        //print("(load using defaults)\n")
+func defaultLoad(showDate: Bool) {                                                  //print("(load using defaults)\n")
     let defaults = UserDefaults.standard
-    let (yr, mo, dy, wkd, hr, mn) = displayDate(Date())
-    let currentDateAsDefault = [yr, mo, dy, wkd, hr, mn] as [Any]
     
     if let components = defaults.array(forKey: "savedLastLoginDate") {
         lastLoginDateComponents = components
-        
         let yearLoaded = lastLoginDateComponents[0] as! Int                         //; print("year loaded: \(yearLoaded)")
         let monthLoaded = lastLoginDateComponents[1] as! String                     //; print("month loaded: \(monthLoaded)")
         let monthLoadedInt = months.firstIndex(of: monthLoaded)! + 1                //; print("int: \(monthLoadedInt)")
@@ -18,13 +15,15 @@ func defaultLoad(showDate: Bool) {                                        //prin
         let hourLoaded = lastLoginDateComponents[4] as! Int                         //; print("hour loaded: \(hourLoaded)")
         let minuteLoaded = lastLoginDateComponents[5] as! Int                       //; print("minute loaded: \(minuteLoaded)")
         
-        lastLoggedInDate = createDateFromLoadedComponents(year: yearLoaded, month: monthLoadedInt, day: dayLoaded, hour: hourLoaded, minute: minuteLoaded)
-        if showDate { print("last login    (unformatted gmt)  \(lastLoggedInDate)")
-                      print("                 (formatted)    \(formattedDateString(lastLoggedInDate, comment: "", short: false))")
+        lastLoggedInDate = dateFromLoadedComponents(year: yearLoaded, month: monthLoadedInt, day: dayLoaded, hour: hourLoaded, minute: minuteLoaded)
+        if showDate {  print("last login       (formatted)    \(formattedDateString(lastLoggedInDate, comment: "", short: false))")
+                       print("              (unformatted gmt)  \(lastLoggedInDate)")
         }
     }
     else {
-        lastLoginDateComponents = currentDateAsDefault ; print("\nfirst login; default previous login date components: \(lastLoginDateComponents)")
+        let (yr, mo, dy, wkd, hr, mn) = displayDate(Date())
+        let currentDateAsDefault = [yr, mo, dy, wkd, hr, mn] as [Any]
+        lastLoginDateComponents = currentDateAsDefault; print("\nfirst login; default previous login date components: \n\(lastLoginDateComponents)")
     }
     timeBlockPaths = defaults.array(forKey: "savedTimeBlockPaths") as? [[Int]] ?? []
     itemDescriptionArrays = defaults.array(forKey: "savedTodoListItems") as? [[String]] ?? []
@@ -47,7 +46,7 @@ func populateDictionaryFromDefaults() {
     }
 }
 
-func createDateFromLoadedComponents(year: Int, month: Int, day: Int, hour: Int, minute: Int) -> Date {
+func dateFromLoadedComponents(year: Int, month: Int, day: Int, hour: Int, minute: Int) -> Date {
     let formatter = DateFormatter()
     formatter.dateFormat = "yyyy/MM/dd HH:mm"
     guard let date = formatter.date(from: "\(year)/\(month)/\(day) \(hour):\(minute)") else {
