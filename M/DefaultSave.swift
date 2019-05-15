9b99e61329a0d63036d332c93ba7b0f44008a5b7
gladiusKatana <garthsnyder@protonmail.com>
2019-05-15 14:08:41 -0400

    Added appends and prints, showing replacement of the saveable array-of-arrays objects timeBlockPaths & itemDescriptionArrays, with nonDefaultTimeBlockPaths & nonDefaultItemDescriptionArrays ⚔️ (...in other words, only time blocks on which the user actually entered a todo-list item (or event; whatever you want to call it) will add their contents to the data saved using the custom model (ie, in DefaultSave.swift) that uses UserDefaults.)

--- M/DefaultSave.swift
+++ M/DefaultSave.swift
@@ -16,34 +16,40 @@ func defaultSave(showDate: Bool) {
     for key in eventsAtIndexPath.keys {
         let (a, b) = key.values                                                                     //; print("key: [\(key)  values \((a, b))")
 
-//        guard let val = eventsAtIndexPath[key] else {print("could not create event at index path key"); return}/*alternate to guard/guard/if: below*/
-//        guard let todo = val.last?.eventDescription else {print("no description at event value"); return}
-//        if todo != defaultEmptEventDescription {
-//            let pathString = "\((a, b)): \(todo)"
-//            nonDefaultTimeBlockPaths.append([a, b])
-//            print(pathString)
-//        }
+        guard let val = eventsAtIndexPath[key] else {print("could not create event at index path key"); return}/*alternate to guard/guard/if: below*/
+        guard let todo = val.last?.eventDescription else {print("no description at event value"); return}
+        if todo != defaultEmptEventDescription {
+            //let pathString = "\((a, b)): \(todo)"                                                 //; print(pathString)
+            nonDefaultTimeBlockPaths.append([a, b])
+        }
         
         timeBlockPaths.append([a, b])                                       //probably temporary, catch-all array (literally: to 'catch 'em all'!)
     }
-    print("\ntime block paths (\(timeBlockPaths.count)): \(timeBlockPaths)")
-    
+//    print("\ntime block paths (\(timeBlockPaths.count)): \(timeBlockPaths)")
+    print("\nnon default time block paths (\(nonDefaultTimeBlockPaths.count)): \(nonDefaultTimeBlockPaths)")
     
     for vals in eventsAtIndexPath.values {
         var eventDescriptions = [String]()
+        
+        if vals.count > 1 || vals.count == 1 && vals[0].eventDescription != defaultEmptEventDescription {
+            var nonDefaultEventDescriptions = [String]()
+            for event in vals {
+                let str = event.eventDescription
+                nonDefaultEventDescriptions.append(str)
+            }
+
+            nonDefaultItemDescriptionArrays.append(nonDefaultEventDescriptions)
+        } //else {print("\n!descriptions array at this time block contains only default (\(defaultEmptEventDescription)), and it's: \(vals[0].eventDescription)")}
+    
         for event in vals {
             let str = event.eventDescription
-            
-            if str != defaultEmptEventDescription { //str = ":)"
-//                nonDefaultItemDescriptionArrays.append(//...
-            }
-            
             eventDescriptions.append(str)
         }
+        
         itemDescriptionArrays.append(eventDescriptions)                     //probably temporary, catch-all array (literally: to 'catch 'em all'!)
     }
-    print("\ntodo list item descriptions (\(itemDescriptionArrays.count)): \(itemDescriptionArrays)")
-    
+//    print("\ntodo list item descriptions (\(itemDescriptionArrays.count)): \(itemDescriptionArrays)")
+    print("\nnon default descriptions (\(nonDefaultItemDescriptionArrays.count)): \(nonDefaultItemDescriptionArrays)")
     
     lastLoginDateComponents = [year, month, day, weekday, hour, minute]
     
