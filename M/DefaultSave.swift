4d5c768ad4d36b385913c7514c5c5132cf5c0d9b
gladiusKatana <garthsnyder@protonmail.com>
2019-05-15 12:18:15 -0400

    Renamed looping constant ('str' to 'event'); other diffs are relevant to next commit ⚔️ (and lines 36 & 37 are just for fun)

--- M/DefaultSave.swift
+++ M/DefaultSave.swift
@@ -2,33 +2,45 @@
 //  smart-timetable  ∙  1st commit Apr. 07, 2019  ∙  Created by Garth Snyder (a.k.a. gladiusKatana ⚔️)
 import UIKit
 
+var nonDefaultPaths = [[Int]]()
+
 func defaultSave(showDate: Bool) {
     
     if showDate {print(formattedDateString(Date(), comment: "saving via defaults, date logged: ", short: false))}
-        
+    
     let defaults = UserDefaults.standard
     
     timeBlockPaths.removeAll(); itemDescriptionArrays.removeAll()
     
     for key in eventsAtIndexPath.keys {
-        let (a, b) = key.values                                             //; print("key: [\(key)  values \((a, b))")
+        let (a, b) = key.values                                                                     //; print("key: [\(key)  values \((a, b))")
         timeBlockPaths.append([a, b])
-        
+
+//        guard let val = eventsAtIndexPath[key] else {print("could not create event at index path key"); return}/*alternate to guard/guard/if: below*/
+//        guard let todo = val.last?.eventDescription else {print("no description at event value"); return}
+//        if todo != defaultEmptEventDescription {
+//            let pathString = "\((a, b)): \(todo)"
+//            nonDefaultPaths.append([a, b])
+//            print(pathString)
+//        }
         
     }
-//    print("\ntime block paths (\(timeBlockPaths.count)): \(timeBlockPaths)")
+    print("\ntime block paths (\(timeBlockPaths.count)): \(timeBlockPaths)")
     
     
     for vals in eventsAtIndexPath.values {
         var eventDescriptions = [String]()
-        for str in vals {
-            eventDescriptions.append(str.eventDescription)
+        for event in vals {
+            var str = event.eventDescription
+            
+            if str == defaultEmptEventDescription {str = ":)"}
+            if str == ":)" {str = defaultEmptEventDescription}
+            
+            eventDescriptions.append(str)
         }
         itemDescriptionArrays.append(eventDescriptions)
     }
-//    print("\ntodo list item descriptions (\(itemDescriptionArrays.count)): \(itemDescriptionArrays)")
-    
-    
+    print("\ntodo list item descriptions (\(itemDescriptionArrays.count)): \(itemDescriptionArrays)")
     lastLoginDateComponents = [year, month, day, weekday, hour, minute]
     
     defaults.set(timeBlockPaths, forKey: "savedTimeBlockPaths")
@@ -36,3 +48,8 @@ func defaultSave(showDate: Bool) {
     defaults.set(lastLoginDateComponents, forKey: "savedLastLoginDate")
 }
 
+/*if let val = eventsAtIndexPath[key] {                                                     // alternative to below section
+    if let todo = val.last?.eventDescription, todo != defaultEmptEventDescription {
+        print(todo)
+    }
+}*/
