a66208c160a586eb4e6d15b23c9e94f9a6567327
gladiusKatana <garthsnyder@protonmail.com>
2019-07-06 19:30:53 -0400

    New file, Fonts.swift

---
+++ V/Fonts.swift
@@ -0,0 +1,7 @@
+//  Fonts.swift
+//  smart-timetable  ∙  1st commit Apr. 07, 2019  ∙  Created by Garth Snyder, aka gladiusKatana ⚔️
+import UIKit
+
+
+var defaultTimetableCellFont = UIFont.systemFont(ofSize: 12, weight: .ultraLight) //UIFont(name: "Helvetica Neue", size: 12)
+
